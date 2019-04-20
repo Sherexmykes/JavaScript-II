7ce159f9d49936ac2b6cad4c8b6dc6a9b017c88c
Sheila R Moore <sherexmykes@github.com>
2019-04-19 20:24:12 -0400

    completed closures.js

--- assignments/closure.js
+++ assignments/closure.js
@@ -1,6 +1,11 @@
 // ==== Challenge 1: Write your own closure ====
 // Write a simple closure of your own creation.  Keep it simple!
-
+function sayHello(name) {
+  let text = 'Hello ' + name;
+  let say = function() { console.log(text); }
+  say();
+}
+sayHello('Sheila');
 
 /* STRETCH PROBLEMS, Do not attempt until you have completed all previous tasks for today's project files */
 
